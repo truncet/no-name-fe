40f63d3bbd8b04a35d6dc0fab9085016a7d7699c
truncet <theonlyeminem@gmail.com>
2021-06-01 12:04:19 +0545

    fetching bookings

--- lib/providers/booking_provider.dart
+++ lib/providers/booking_provider.dart
@@ -11,9 +11,14 @@ class BookServiceProvider with ChangeNotifier {
   String _uId;
   double _estimatedCost;
   DateTime _dateTime;
-  String _status;
+  Status _status;
+  String _bookId;
 
   List<Booking> _bookings = [];
+
+  List<Booking> get bookings {
+    return _bookings;
+  }
 
   void update(AuthProvider tok, BookServiceProvider service) async {
     await tok.autoToken();
@@ -25,26 +30,53 @@ class BookServiceProvider with ChangeNotifier {
     _status = service == null ? '' : service._status;
   }
 
-  Future<void> fetchBookings() async {}
+  void addBookings(String response) {}
 
-  Future<void> bookServices(
-    String id,
-    String uId,
-    double price,
+  Future<void> fetchBookings() async {
+    final url = Uri.parse("http://192.168.1.20:5000/booking");
+    final headers = {
+      'Content-type': 'application/json',
+      'Authorization': 'Bearer $token'
+    };
+    final response = await http.get(url, headers: headers);
+    if (response.statusCode == 200) {
+      addBookings(response.body);
+    }
+
+    print(json.decode(response.body));
+  }
+
+  Future<Booking> bookServices(
+    Booking booking,
   ) async {
     final url = Uri.parse("http://192.168.1.20:5000/booking/");
     final headers = {
       'Content-type': 'application/json',
       'Authorization': 'Bearer $token'
     };
+    _estimatedCost = booking.price * booking.hours;
+    _sId = booking.sId;
+    _uId = booking.uId;
+    _dateTime = booking.time;
+    _bookId = booking.id;
+    _status = Status.Pending;
 
     final response = await http.post(url,
         headers: headers,
         body: json.encode({
-          'id': id,
-          'uId': uId,
-          'price': price.toString(),
+          'sId': _sId,
+          'uId': _uId,
+          'price': _estimatedCost.toString(),
+          'date': _dateTime.toIso8601String(),
+          'bookid': _bookId,
+          'status': _status.toString(),
         }));
+    if (response.statusCode == 200) {
+      notifyListeners();
+      return booking;
+      //_addNewBooking(booking);
+    }
     notifyListeners();
+    return Booking();
   }
 }
